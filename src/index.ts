017045568eabaa025a648ddf09e315955e5f98dc
Daniel <daniel.bencz@gmail.com>
2021-03-22 21:56:21 +0100

    first fetch to sportradar api

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,23 @@
 import express from 'express'
-const app = express()
-const port = process.env.PORT || 4040
+import fetch from 'node-fetch'
+import { Response } from 'express'
 
-app.get('/', (_, res) => {
-    res.status(200).send('hello')
-})
-app.listen(port, () => console.log(`Running on port ${port}`))
+const main = (): void => {
+    const app = express()
+    const port = process.env.PORT || 4040
+
+    app.get('/', (_, res: Response) => {
+        res.status(200).send('hello')
+    })
+
+    app.get('/tournaments', async (_, res: Response) => {
+        const tournamentUri = 'https://cp.fn.sportradar.com/common/en/Etc:UTC/gismo/fixtures_tournament/{_tid}/2021'
+        const tournamentsResponse = await fetch(tournamentUri)
+        const tournamentsJson = await tournamentsResponse.json()
+        res.json(tournamentsJson)
+    })
+
+    app.listen(port, () => console.log(`Running on port ${port}`))
+
+}
+main()
