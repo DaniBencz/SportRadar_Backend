6f677bfa6943988494b1a21726f436accf4d6021
Daniel Bencz <daniel.bencz@gmail.com>
2021-03-25 10:18:25 +0100

    all matches unsorted

--- src/index.ts
+++ src/index.ts
@@ -1,59 +1,19 @@
 import express from 'express'
-import fetch from 'node-fetch'
 import { Response } from 'express'
-import { getTournamentNamesByID } from './services'
+import { getLastNMatchesGroupedByTournament } from './services'
 
-const main = (): void => {
-    // dynamic imports?
+const app = express()
+const port = process.env.PORT || 4040
 
-    const app = express()
-    const port = process.env.PORT || 4040
+app.use(express.static('public'))
 
-    app.use(express.static('public'))
+app.get('/', (_, res: Response) => {
+    res.status(200).sendFile('index.html')
+})
 
-    app.get('/', (_, res: Response) => {
-        res.status(200).sendFile('index.html')
-    })
+app.get('/matches', async (_, res: Response) => {
+    const lastFiveMatchesGroupedByTournament = await getLastNMatchesGroupedByTournament(5)
+    res.json(lastFiveMatchesGroupedByTournament)
+})
 
-    app.get('/matches', async (_, res: Response) => {
-        const tournamentsAPI = 'https://cp.fn.sportradar.com/common/en/Etc:UTC/gismo/config_tournaments/1/17'
-        const tournamentNamesById = await getTournamentNamesByID(tournamentsAPI)
-
-        const extensiveMatchDataByTournamentPromises = Object.keys(tournamentNamesById).map((id: string) => {
-            const matchesUri = `https://cp.fn.sportradar.com/common/en/Etc:UTC/gismo/fixtures_tournament/${id}/2021`
-            return fetch(matchesUri)
-        })
-
-        const extensiveMatchDataByTournamentJson = (await Promise.all(extensiveMatchDataByTournamentPromises)).map((matchData) => {
-            return matchData.json()
-        })
-
-        const matchDataByTournamentJson = (await Promise.all(extensiveMatchDataByTournamentJson)).map((data: any) => {
-            if (data?.doc && data?.doc[0]?.data?.matches) return data.doc[0].data.matches
-            else return []
-        })
-
-        res.json(matchDataByTournamentJson)
-
-        /*
-        {
-            Bundesliga: {
-                id1: {
-                    // details
-                },
-                id2: {
-                    // details
-                }
-            },
-            OFB: {
-                id3: {}
-            }
-
-        }
-        */
-    })
-
-    app.listen(port, () => console.log(`Running on port ${port}`))
-
-}
-main()
+app.listen(port, () => console.log(`Running on port ${port}`))
--- src/services.ts
+++ src/services.ts
@@ -1,6 +1,8 @@
 import fetch from 'node-fetch'
 
-export const getTournamentNamesByID = async (uri: string) => {
+interface Itournament { _id: number, name: string }
+
+const getTournamentNamesByID = async (uri: string) => {
     const tournamentsResponse = await fetch(uri)
     const tournamentsJson = await tournamentsResponse.json()
     const {
@@ -9,8 +11,6 @@ export const getTournamentNamesByID = async (uri: string) => {
         cuptrees: cupTreesData
     } = tournamentsJson.doc[0].data
 
-    interface Itournament { _id: number, name: string }
-
     const tournamentNamesById = tournamentsData.reduce((acc: Itournament, cur: Itournament) => {
         return { ...acc, ...{ [cur._id]: cur.name } }
     }, {})
@@ -18,13 +18,71 @@ export const getTournamentNamesByID = async (uri: string) => {
     Object.keys(uniqueTournamentsData).forEach((id: string) => {
         if (!tournamentNamesById[id]) tournamentNamesById[id] = uniqueTournamentsData[id].name
     })
-    
+
     Object.keys(cupTreesData).forEach((id: string) => {
         if (!tournamentNamesById[id]) tournamentNamesById[id] = cupTreesData[id].name
     })
 
     return tournamentNamesById
 }
+
+const convertMatchesObjectToArray = (matches: any) => {
+    const matchesArray = Object.keys(matches).map((key) => {
+        return matches[key]
+    })
+    return matchesArray
+}
+
+export const getLastNMatchesGroupedByTournament = async (numberOfMatches: number) => {
+    const tournamentsAPI = 'https://cp.fn.sportradar.com/common/en/Etc:UTC/gismo/config_tournaments/1/17'
+    const tournamentNamesById = await getTournamentNamesByID(tournamentsAPI)
+
+    const extensiveMatchDataByTournamentPromises = Object.keys(tournamentNamesById).map((id: string) => {
+        const matchesUri = `https://cp.fn.sportradar.com/common/en/Etc:UTC/gismo/fixtures_tournament/${id}/2021`
+        return fetch(matchesUri)
+    })
+
+    const extensiveMatchDataByTournamentJson = (await Promise.all(extensiveMatchDataByTournamentPromises))
+        .map((response) => response.json())
+
+    const matchesGroupedByTournamentJson = (await Promise.all(extensiveMatchDataByTournamentJson)).reduce((acc, cur) => {
+        if (cur?.doc[0]?.data?.matches) {
+            const isMatchesPropertyEmpty = cur?.doc[0]?.data?.matches instanceof Array
+            if (!isMatchesPropertyEmpty) return [...acc, cur?.doc[0]?.data?.matches]
+            else return acc
+        } else return acc
+    }, [])
+
+    const allMatchesUnsorted = matchesGroupedByTournamentJson.reduce((acc: [], cur: any) => {
+        return [...acc, ...convertMatchesObjectToArray(cur)]
+    }, [])
+
+    const allMatchesSorted = [...allMatchesUnsorted].sort((a, b) => {
+        return a.time.uts - b.time.uts
+    })
+
+    return allMatchesSorted
+}
+
+
+
+
+/*
+{
+    Bundesliga: {
+        id1: {
+            // details
+        },
+        id2: {
+            // details
+        }
+    },
+    OFB: {
+        id3: {}
+    }
+
+}
+*/
 
 /*
 Grouped by tournament
