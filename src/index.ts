3896a7657d739d1e48183d0ba5fab315ea8ba427
Daniel Bencz <daniel.bencz@gmail.com>
2021-03-26 23:29:43 +0100

    new build

--- src/index.ts
+++ src/index.ts
@@ -8,18 +8,18 @@ const port = process.env.PORT || 4040
 app.use(express.static('public'))
 
 app.get('/', (_, res: Response) => {
-    res.status(200).sendFile('index.html')
+  res.status(200).sendFile('index.html')
 })
 
 app.get('/matches', async (_, res: Response) => {
-    try {
-        const lastFiveMatchesGroupedByTournament = await getLastNMatchesGroupedByTournament(5)
-        res.json(lastFiveMatchesGroupedByTournament)
-    }
-    catch (e) {
-        console.log(e)
-        res.status(500).json({ message: 'Server error, please try again later' })
-    }
+  try {
+    const lastFiveMatchesGroupedByTournament = await getLastNMatchesGroupedByTournament(5)
+    res.json(lastFiveMatchesGroupedByTournament)
+  }
+  catch (e) {
+    console.log(e)
+    res.status(500).json({ message: 'Server error, please try again later' })
+  }
 })
 
 app.listen(port, () => console.log(`Running on port ${port}`))
