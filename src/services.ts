8a07b7cba9d474c658903e291d4fe143d9bb8e94
Daniel Bencz <daniel.bencz@gmail.com>
2021-03-27 18:13:12 +0100

    refactoring and comments

--- src/services.ts
+++ src/services.ts
@@ -52,35 +52,37 @@ const getTournamentNamesAndIDs = async (uri: string): Promise<{ [key: string]: s
     if (!tournamentNamesAndIDs[id]) tournamentNamesAndIDs[id] = cupTreesData[id].name
   })
 
-  return tournamentNamesAndIDs
+  return tournamentNamesAndIDs // an object with tournament name as key, and tournament id as value
 }
 
-const getMatchesData = (tournamentNamesAndIDs: { [key: string]: string }) => {
-  return Object.keys(tournamentNamesAndIDs).map((id: string) => {
+const getAllTournamentsData = async (tournamentNamesAndIDs: { [key: string]: string }) => {
+  const responseData = Object.keys(tournamentNamesAndIDs).map((id: string) => {
     const matchesUri = `https://cp.fn.sportradar.com/common/en/Etc:UTC/gismo/fixtures_tournament/${id}/2021`
     return fetch(matchesUri)
   })
+
+  return (await Promise.all(responseData)).map((response) => response.json())
 }
 
-const convertMatchesResponseToJson = async (responseArray: Promise<Response>[]) => {
-  return (await Promise.all(responseArray)).map((response) => response.json())
-}
-
-const filterMatchesDataFromExtensiveData = async (extensiveData: Promise<any>[]): Promise<{ [key: string]: IMatchRaw }[]> => {
-  return (await Promise.all(extensiveData)).reduce((acc, cur) => {
+const filterMatchesDataFromTournamentData = async (detailedTournamentData: Promise<any>[]): Promise<{ [key: string]: IMatchRaw }[]> => {
+  return (await Promise.all(detailedTournamentData)).reduce((acc, cur) => {
     if (cur?.doc[0]?.data?.matches) {
+      // if no match data is available from the tournament, it comes as empty array, else it is an object:
       const isMatchesPropertyEmpty = cur?.doc[0]?.data?.matches instanceof Array
+
       if (!isMatchesPropertyEmpty) return [...acc, cur?.doc[0]?.data?.matches]
       else return acc
     } else return acc
   }, [])
 }
 
+// matches are originally listed in an object, where match-id is the key, and match-data is value
+// here, this object is converted into an array, containing the match-datas as elements
 export const convertMatchesObjectToArray = (matches: { [key: string]: IMatchRaw }) => {
   return Object.keys(matches).map((key) => matches[key])
 }
 
-const getAllMatchesInOneArray = (matchesGroupedByTournament: { [key: string]: IMatchRaw }[]) => {
+const pushAllMatchesInOneSingleArray = (matchesGroupedByTournament: { [key: string]: IMatchRaw }[]) => {
   return matchesGroupedByTournament.reduce((acc: IMatchRaw[], cur: { [key: string]: IMatchRaw }) => {
     return [...acc, ...convertMatchesObjectToArray(cur)]
   }, [])
@@ -94,16 +96,21 @@ const getLastNMatches = (numberOfMatches: number, allMatches: IMatchRaw[]): IMat
   let results: IMatchRaw[] = []
   let now = Math.round(new Date().getTime() / 1000)
 
-  for (let i = 0; i <= 180; i++) {
+  // looping backwards in time from future matches data to present, and past
+  // saving the N recent-most matches to a new array, and exit
+  for (let i = 0; i <= allMatches.length; i++) {
     if (allMatches[i].time.uts <= now) results.push(allMatches[i])
     if (results.length >= numberOfMatches) break
   }
   return results
 }
 
+// the comment data-field gets verbose sometimes
+// here, this string is broken down to an array of 'widget-sized'  pieces of info
+// according to this example format: '1:0 (18.) M.Toro'
 const parseCommentStringToEventsArray = (comment: string) => {
   const eventsRaw = comment.split(',').map((comment) => comment.trim())
-  const regex = /\d:\d\s\(\d\d\.\)\s[a-zA-Z0-9.]+/g // matches: '1:0 (18.) M.Toro' format
+  const regex = /\d:\d\s\(\d\d\.\)\s[a-zA-Z0-9.]+/g
 
   const events = eventsRaw.reduce((acc: string[], cur: string) => {
     const event = cur.match(regex)
@@ -113,44 +120,46 @@ const parseCommentStringToEventsArray = (comment: string) => {
   return events
 }
 
-const convertDataToExpectedOutput = (matchesData: IMatchRaw[], tournamentNamesAndIDs: { [key: string]: string }) => {
-  let tournaments: { [key: string]: IMatchProcessed[] } = {}
+const filterRequiredMatchesDataFieldsAndGroupMatchesByTournament =
+  (matchesData: IMatchRaw[], tournamentNamesAndIDs: { [key: string]: string }) => {
+    let tournaments: { [key: string]: IMatchProcessed[] } = {}
 
-  matchesData.forEach((match) => {
-    const { _tid, time, teams, result, comment } = match
-    const tournamentName = tournamentNamesAndIDs[_tid]
-    const events = parseCommentStringToEventsArray(comment)
+    matchesData.forEach((match) => {
+      const { _tid, time, teams, result, comment } = match
+      const tournamentName = tournamentNamesAndIDs[_tid]
+      const events = parseCommentStringToEventsArray(comment)
 
-    const processedMatchData = {
-      dateAndTime: `${time.date}: ${time.time}`,
-      teams: {
-        home: teams.home.name,
-        away: teams.away.name
-      },
-      score: {
-        home: result.home,
-        away: result.away
-      },
-      events: events,
-    }
+      const processedMatchData: IMatchProcessed = {
+        dateAndTime: `${time.date}: ${time.time}`,
+        teams: {
+          home: teams.home.name,
+          away: teams.away.name
+        },
+        score: {
+          home: result.home,
+          away: result.away
+        },
+        events: events,
+      }
 
-    if (!tournaments[tournamentName]) {
-      tournaments[tournamentName] = [processedMatchData]
-    } else tournaments[tournamentName].push(processedMatchData)
-  })
-  return tournaments
-}
+      if (!tournaments[tournamentName]) {
+        tournaments[tournamentName] = [processedMatchData]
+      } else tournaments[tournamentName].push(processedMatchData)
+    })
+    return tournaments
+  }
 
 export const getLastNMatchesGroupedByTournament = async (numberOfMatches: number) => {
   const tournamentsAPI = 'https://cp.fn.sportradar.com/common/en/Etc:UTC/gismo/config_tournaments/1/17'
   const tournamentNamesAndIDs = await getTournamentNamesAndIDs(tournamentsAPI)
-  const extensiveMatchDataGroupedByTournamentResponse = getMatchesData(tournamentNamesAndIDs)
-  const extensiveMatchDataGroupedByTournamentJson = await convertMatchesResponseToJson(extensiveMatchDataGroupedByTournamentResponse)
-  const matchesGroupedByTournament = await filterMatchesDataFromExtensiveData(extensiveMatchDataGroupedByTournamentJson)
 
-  const allMatchesUnsorted = getAllMatchesInOneArray(matchesGroupedByTournament)
+  const detailedTournamentsData = await getAllTournamentsData(tournamentNamesAndIDs)
+  const matchesGroupedByTournament = await filterMatchesDataFromTournamentData(detailedTournamentsData)
+
+  const allMatchesUnsorted = pushAllMatchesInOneSingleArray(matchesGroupedByTournament)
   const allMatchesSorted = sortAllMatchesByTimeDescending(allMatchesUnsorted)
+
   const lastNMatches = getLastNMatches(numberOfMatches, allMatchesSorted)
-  const normalizedResult = convertDataToExpectedOutput(lastNMatches, tournamentNamesAndIDs)
+  const normalizedResult = filterRequiredMatchesDataFieldsAndGroupMatchesByTournament(lastNMatches, tournamentNamesAndIDs)
   return normalizedResult
 }
