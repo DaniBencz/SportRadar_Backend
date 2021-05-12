ea9dd69ceec0cad2d5581937ec64861810de397b
Daniel Bencz <daniel.bencz@gmail.com>
2021-05-12 09:14:30 +0200

    async pipeline

--- src/services.ts
+++ src/services.ts
@@ -4,15 +4,14 @@ import {
   IMatchRaw, IMatchProcessed, ITournaments,
 } from './serviceTypes';
 
-const getTournaments = async () => {
+const getTournaments = async (): Promise<any> => {
   const uri = 'https://cp.fn.sportradar.com/common/en/Etc:UTC/gismo/config_tournaments/1/17';
   const tournamentsResponse = await fetch(uri);
   const tournamentsJson = await tournamentsResponse.json();
   return tournamentsJson.doc[0].data;
 };
 
-const getTournamentIDsAndNames = async (dataPromise: any): Promise<{ [key: number]: string; }> => {
-  const data = await dataPromise;
+const getTournamentIDsAndNames = async (data: any): Promise<{ [key: number]: string; }> => {
   const {
     tournaments,
     uniquetournaments,
@@ -38,21 +37,22 @@ const getTournamentIDsAndNames = async (dataPromise: any): Promise<{ [key: numbe
   return tournamentIDsAndNames; // an object with tournament id as key, and tournament name as value
 };
 
-const getAllTournamentsData = async (data: Promise<{ [key: number]: string; }>) => {
-  const tournamentIDsAndNames = await data;
+const getAllTournamentsData = async (tournamentIDsAndNames: { [key: number]: string; }) => {
   const responseData = Object.keys(tournamentIDsAndNames).map(async (id) => {
     const matchesUri = `https://cp.fn.sportradar.com/common/en/Etc:UTC/gismo/fixtures_tournament/${id}/2021`;
 
-    // below would be better solved with some kind of Promise.all
-    return { tName: tournamentIDsAndNames[Number(id)], data: await (await fetch(matchesUri)).json() };
+    return {
+      tName: tournamentIDsAndNames[Number(id)],
+      // below would be better solved with some kind of Promise.all
+      data: await (await fetch(matchesUri)).json(),
+    };
   });
 
   return Promise.all(responseData);
 };
 
-const filterMatchesDataFromTournamentData = async (data: Promise<any[]>): Promise<{ [key: number]: IMatchRaw; }[]> => {
-  const detailedTournamentData = await data;
-  return detailedTournamentData.reduce((acc, cur) => {
+const filterMatchesDataFromTournamentData = async (detailedTournamentData: any[]): Promise<{ [key: number]: IMatchRaw; }[]> =>
+  detailedTournamentData.reduce((acc, cur) => {
     const matches = cur.data?.doc[0]?.data?.matches;
     if (matches) {
       // if no match data is available from the tournament, it comes as empty array, else it is an object:
@@ -69,21 +69,14 @@ const filterMatchesDataFromTournamentData = async (data: Promise<any[]>): Promis
       return acc;
     } return acc;
   }, []);
-};
 
-const pushAllMatchesInOneSingleArray = async (data: Promise<IMatchRaw[][]>) => {
-  const matchesGroupedByTournament = await data;
-  return matchesGroupedByTournament.reduce((acc: IMatchRaw[], cur: IMatchRaw[]) =>
-    [...acc, ...cur], []);
-};
+const pushAllMatchesInOneSingleArray = async (matchesGroupedByTournament: IMatchRaw[][]) => matchesGroupedByTournament
+  .reduce((acc: IMatchRaw[], cur: IMatchRaw[]) => [...acc, ...cur], []);
 
-export const sortAllMatchesByTimeDescending = async (unsortedMatches: Promise<IMatchRaw[]>) => {
-  const matches = await unsortedMatches;
-  return matches.sort((a, b) => b.time.uts - a.time.uts);
-};
+export const sortAllMatchesByTimeDescending = async (unsortedMatches: IMatchRaw[]) => unsortedMatches
+  .sort((a, b) => b.time.uts - a.time.uts);
 
-const getLastNMatches = async (allMatches: IMatchRaw[]): Promise<IMatchRaw[]> => {
-  const matches = await allMatches;
+const getLastNMatches = async (matches: IMatchRaw[]): Promise<IMatchRaw[]> => {
   const results: IMatchRaw[] = [];
   const now = Math.round(new Date().getTime() / 1000);
 
@@ -111,11 +104,10 @@ const parseCommentStringToEventsArray = (comment: string): { eid: number, event:
   return events;
 };
 
-const filterRequiredMatchesDataFieldsAndGroupMatchesByTournament = async (data: Promise<IMatchRaw[]>) => {
-  const matchesData = await data;
+const filterRequiredMatchesDataFieldsAndGroupMatchesByTournament = async (data: IMatchRaw[]) => {
   const tournaments: ITournaments = {};
 
-  matchesData.forEach((match) => {
+  data.forEach((match) => {
     const {
       _id, _tid, time, teams, result, comment, tName,
     } = match;
@@ -145,15 +137,15 @@ const filterRequiredMatchesDataFieldsAndGroupMatchesByTournament = async (data: 
   return tournaments;
 };
 
-const logger = async <T>(promise: T) => {
-  const data = await promise;
+const logger = async <T>(data: T) => {
   // eslint-disable-next-line no-console
   console.log('logger: ', JSON.stringify(data, null, 2));
   return data;
 };
 
 // https://stackoverflow.com/questions/65154695/typescript-types-for-a-pipe-function
-const pipe = (...fns: any[]) => (val: any = []) => fns.reduce((acc, cur) => cur(acc), val);
+const pipe = (...fns: any[]) => (val: any = []) => fns.reduce(async (acc, cur) => cur(await acc), val);
+
 const getLastFiveMatchesGroupedByTournament = pipe(
   getTournaments,
   getTournamentIDsAndNames,
